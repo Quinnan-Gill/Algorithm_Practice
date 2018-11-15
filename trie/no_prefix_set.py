82b481a93d8e8e26218d48fabb9981dd83f18fd1
Quinnan Gill <quinnan.gill@gmail.com>
2018-11-14 23:09:09 -0500

    Added some trie problems

---
+++ trie/no_prefix_set.py
@@ -0,0 +1,29 @@
+from sys import stdin
+from collections import defaultdict
+
+def TrieNode:
+    def __init__(self):
+        self.children = [None] * 26
+    def
+
+def add(trie, word):
+    if word == '':
+    	if len(trie) > 0:
+            # current word is a prefix of a previous word
+            return False
+        trie[word] = ''
+        return True
+    if '' in trie:
+    	# a prefix of the current word is a previous word
+        return False
+    return add(trie[word[0]], word[1:])
+
+stdin.readline()
+trie = Trie()
+for line in stdin:
+    line = line.strip()
+    if not add(trie, line):
+        print "BAD SET"
+        print line
+        sys.exit()
+print "GOOD SET"
